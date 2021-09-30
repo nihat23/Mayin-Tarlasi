3e24fcd992a7c5651c8a24d0837e54a1b1892329
nihat23 <62906442+nihat23@users.noreply.github.com>
2021-09-30 12:09:18 +0300

    Mayın Tarlası
    Mayın Tarlası

---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Mayın_Tarlası
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1Renk = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button2YenidenOyna = new System.Windows.Forms.Button();
+            this.label2secilen = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4bulunan = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // button1Renk
+            // 
+            this.button1Renk.BackColor = System.Drawing.Color.SandyBrown;
+            this.button1Renk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1Renk.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1Renk.Location = new System.Drawing.Point(3, 362);
+            this.button1Renk.Margin = new System.Windows.Forms.Padding(4);
+            this.button1Renk.Name = "button1Renk";
+            this.button1Renk.Size = new System.Drawing.Size(105, 33);
+            this.button1Renk.TabIndex = 0;
+            this.button1Renk.Text = "Renk";
+            this.button1Renk.UseVisualStyleBackColor = false;
+            this.button1Renk.Click += new System.EventHandler(this.button1Renk_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(13, 13);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(661, 290);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // button2YenidenOyna
+            // 
+            this.button2YenidenOyna.BackColor = System.Drawing.Color.SandyBrown;
+            this.button2YenidenOyna.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2YenidenOyna.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2YenidenOyna.Location = new System.Drawing.Point(700, 13);
+            this.button2YenidenOyna.Margin = new System.Windows.Forms.Padding(4);
+            this.button2YenidenOyna.Name = "button2YenidenOyna";
+            this.button2YenidenOyna.Size = new System.Drawing.Size(145, 33);
+            this.button2YenidenOyna.TabIndex = 2;
+            this.button2YenidenOyna.Text = "Yeniden oyna";
+            this.button2YenidenOyna.UseVisualStyleBackColor = false;
+            this.button2YenidenOyna.Click += new System.EventHandler(this.button2YenidenOyna_Click);
+            // 
+            // label2secilen
+            // 
+            this.label2secilen.AutoSize = true;
+            this.label2secilen.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2secilen.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label2secilen.Location = new System.Drawing.Point(758, 113);
+            this.label2secilen.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2secilen.Name = "label2secilen";
+            this.label2secilen.Size = new System.Drawing.Size(30, 31);
+            this.label2secilen.TabIndex = 4;
+            this.label2secilen.Text = "?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(708, 155);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mayın Sayısı";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(707, 68);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 33);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Seçilen";
+            // 
+            // label4bulunan
+            // 
+            this.label4bulunan.AutoSize = true;
+            this.label4bulunan.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4bulunan.ForeColor = System.Drawing.Color.DarkRed;
+            this.label4bulunan.Location = new System.Drawing.Point(758, 209);
+            this.label4bulunan.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4bulunan.Name = "label4bulunan";
+            this.label4bulunan.Size = new System.Drawing.Size(30, 31);
+            this.label4bulunan.TabIndex = 6;
+            this.label4bulunan.Text = "?";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(710, 382);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(68, 17);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "NihatBeyi";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(877, 408);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label4bulunan);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2secilen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2YenidenOyna);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.button1Renk);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Mayın Tarlası";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1Renk;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button2YenidenOyna;
+        private System.Windows.Forms.Label label2secilen;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4bulunan;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mayın_Tarlası
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1Renk_Click(object sender, EventArgs e)
+        {
+            DialogResult arkarenk = colorDialog1.ShowDialog();
+            if(arkarenk== DialogResult.OK)
+            {
+                this.BackColor = colorDialog1.Color;
+            }
+        }
+
+
+
+        private void button2YenidenOyna_Click(object sender, EventArgs e)
+        {
+            secilen = 0;
+            bulunan = 0;
+            butonuret();
+
+        }
+        private void butonuret()
+        {
+            flowLayoutPanel1.Controls.Clear();
+
+            Random rnd = new Random();
+            
+            List<int> mayinlar = new List<int>();
+            
+            for(int i=1; mayinlar.Count<40; i++)
+            {
+                int sayi = rnd.Next(1, 114);
+                if (!mayinlar.Contains(sayi))
+                {
+                    mayinlar.Add(sayi);
+                }
+            }
+
+            for(int bos_hucre=1; bos_hucre < 113; bos_hucre++)
+            {
+                Button btn = new Button();
+                btn.Size = new System.Drawing.Size(35, 35);
+                btn.Text = bos_hucre.ToString();
+
+                if (mayinlar.Contains(bos_hucre))
+                {
+                    btn.Tag = true;
+
+                }
+                else
+                {
+                    btn.Tag = false;
+                }
+                btn.Click += Btn_Click;
+
+                flowLayoutPanel1.Controls.Add(btn);
+            }
+        }
+
+
+        int secilen = 0;
+        int bulunan = 0;
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button basilanButon =(Button) sender;
+            bool mayin = (bool)basilanButon.Tag;
+
+            if (mayin)
+            {
+                basilanButon.BackColor = Color.Red;
+                basilanButon.Enabled = false;
+                MessageBox.Show("Mayına Bastınız", "TEHLİKE", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                bulunan++;
+                
+            }
+            else
+            {
+                basilanButon.BackColor = Color.Green;
+                basilanButon.Enabled = false;
+                secilen++;
+            }
+            label4bulunan.Text = bulunan.ToString();
+            label2secilen.Text = secilen.ToString();
+
+            if (bulunan >=30)
+            {
+                DialogResult soruvecevap = MessageBox.Show("Bütün Mayınlar Patladı..!", "Yeniden Başlatmak istermisin", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (soruvecevap == DialogResult.OK)
+                {
+                    secilen = 0;
+                    bulunan = 0;
+                    butonuret();
+
+                }
+                else
+                {
+                    secilen = 0;
+                    bulunan = 0;
+                    butonuret();
+                    
+                }
+
+
+            }
+
+
+        }
+
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            butonuret();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.facebook.com/n.beyi");
+        }
+    }
+}
